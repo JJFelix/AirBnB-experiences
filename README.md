ddf3876f0c38a30cebda6713633f35f88248a542
MiniThought17 <felixomwenga441@gmail.com>
2023-01-25 12:27:06 +0300

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## A static AirBnB experiences page using React
+
+View the site at https://jjfelix.github.io/AirBnB-experiences/
--- src/data.jsx
+++ src/data.jsx
@@ -4,7 +4,7 @@ export default [
         title: "Who did you say is the G.O.A.T?",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "/assets/person2.png",
+        coverImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1yRW6MYqMBlywlezwT-VNuJyBfXgx39iYNQ&usqp=CAU" , //"/assets/person2.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -17,7 +17,7 @@ export default [
         title: "Know how to ride a bike?",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "/assets/person1.png",
+        coverImg: "https://images.pexels.com/photos/4279653/pexels-photo-4279653.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", //"/assets/person1.png",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -30,51 +30,38 @@ export default [
         title: "Formula1 back soon",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "/assets/person3.png",
+        coverImg:"https://i.pinimg.com/736x/ab/56/37/ab563777c99d90fa4f430f41a300beb8--f-racing-formula-one.jpg" , //"/assets/person3.png",
         stats: {
-            rating: 4.8,
-            reviewCount: 2
+            rating: 4.5,
+            reviewCount: 11
         },
-        location: "USA",
+        location: "Online",
         openSpots: 3,
     },
     {
         id: 4,
-        title: "Formula1 back soon",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "/assets/person3.png",
+        title: "Maasai Mara",
+        description: "The Maasai community is rich in culture. Located in Kenya, East Africa, they have the most longstanding cultures in the region, including pastoral nomadism and wonderful food.",
+        price: 120,
+        coverImg:"https://i.pinimg.com/236x/1d/92/4b/1d924b1ca368e73cb36ab374fa623172.jpg" , //"/assets/person3.png",
         stats: {
             rating: 4.8,
-            reviewCount: 2
+            reviewCount: 113
         },
-        location: "Online",
+        location: "Kenya",
         openSpots: 42,
     },
     {
         id: 5,
-        title: "Formula1 back soon",
+        title: "Hindu cultural experience",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "/assets/person3.png",
+        coverImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJRUxlrlHlKSTCyjYl7nequNiBB9iGF60u9pFAcAOQhmBNhlJU7b93UV-WZtmkj8HGMkE&usqp=CAU",// "/assets/person3.png",
         stats: {
             rating: 4.8,
-            reviewCount: 2
+            reviewCount: 14,
         },
-        location: "USA",
+        location: "India",
         openSpots: 0,
-    },
-    {
-        id: 6,
-        title: "Formula1 back soon",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "/assets/person3.png",
-        stats: {
-            rating: 4.8,
-            reviewCount: 2
-        },
-        location: "Online",
-        openSpots: 4,
     }
 ]
